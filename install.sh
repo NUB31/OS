82a0c6f840ae66327f80ebb44512675f60ba17e8
NUB31 <59693115+NUB31@users.noreply.github.com>
2023-03-07 23:49:16 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+sudo apt update
+sudo apt upgrade
+
+sudo apt-get install -y awesome neofetch konsole firefox zsh dolphin nitrogen compton breeze-icon-theme lxinput neovim terminator xorg
+
+chsh -s /bin/zsh
+
+touch .xinitrc
+echo "exec awesome" > .xinitrc
+
+touch .zprofile
+echo "startx" > .zprofile
+
+mkdir  ~/.config/awsome
+
+touch rc.lua
+cat /etc/xdg/awesome/rc.lua > rc.lua
