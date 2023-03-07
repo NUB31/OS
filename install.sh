3d3b99e2ab35c3c151421e87951f3bedac829fe8
NUB31 <59693115+NUB31@users.noreply.github.com>
2023-03-08 00:00:30 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
-sudo apt-get -y update
-sudo apt-get -y upgrade
+USER_HOME=$(eval echo ~${SUDO_USER})
+cd ${USER_HOME}
+
+sudo apt update
+sudo apt upgrade
 
 sudo apt-get install -y awesome neofetch konsole firefox zsh dolphin nitrogen compton breeze-icon-theme lxinput neovim terminator xorg
 
 chsh -s /bin/zsh
 
 touch .xinitrc
-echo "exec awesome" > ~/.xinitrc
+echo "exec awesome" > .xinitrc
 
 touch .zprofile
-echo "startx" > ~/.zprofile
+echo "startx" > .zprofile
 
-mkdir  ~/.config/awsome
+mkdir  .config/awsome
 
 touch rc.lua
-cat /etc/xdg/awesome/rc.lua > ~/rc.lua
+cat /etc/xdg/awesome/rc.lua > rc.lua
