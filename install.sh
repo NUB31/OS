2bb696d10a88b9b58ed33752f646361296f8c6a5
nub31 <oliverlhs@outlook.com>
2023-03-08 18:25:54 +0000

    added xorg to install

--- install.sh
+++ install.sh
@@ -1,31 +1,40 @@
 #!/bin/sh
+
+# Run updates
 sudo apt update
 sudo apt upgrade
 
+# Install packages and dependencies
 sudo apt-get install -y awesome terminator build-essential libpam0g-dev libxcb-xkb-dev zsh xorg
 
+# Download ly display manager
 git clone https://github.com/fairyglade/ly --recurse-submodules
 
+# Compily ly
 cd ly
 make
 make install installsystemd
 
+# Enable ly
 systemctl enable ly.service
 
 cd ../
 
+# Create startup config
 touch .xinitrc
 echo "exec awesome" > .xinitrc
 
 touch .zprofile
 echo "startx" > .zprofile
 
+# Apply awesome config
 mkdir .config
 mkdir .config/awesome
 
 touch .config/awesome/rc.lua
 cat os/rc.lua > .config/awesome/rc.lua
 
-chsh -s /etc/zsh
+sudo chsh -s /etc/zsh
 
+# Reboot into ly
 reboot
